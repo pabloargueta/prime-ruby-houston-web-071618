1eb9ad83b0cc91882b06d8c2da6c910b32d847c7
Pablo Argueta <pabloargueta27@gmail.com>
2018-06-19 02:56:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,16 @@
 # Add  code here!
 
 def prime?(integer)
-  
- #create a range of numbers from 2 to integer  - 1
-  
-  for num in 2..(integer-1)do
-    if num <= 1 
-      return false
-    end
-    return false if integer % num == 0
-    
-  end
+
+n = 2
+
+while n < integer
+
+  return false if integer % n == 0
+ 
+ n += 1
+ 
+ return true
 
   
 end
