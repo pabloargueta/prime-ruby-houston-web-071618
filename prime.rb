bb0dc04488dd731e8891545f927a60b489cbaff7
Pablo Argueta <pabloargueta27@gmail.com>
2018-06-19 02:55:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(integer)
   
  #create a range of numbers from 2 to integer  - 1
   
-  for num in 2..(integer-1) do
+  for num in 2..(integer-1)do
     if num <= 1 
       return false
     end
