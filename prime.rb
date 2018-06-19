7c7b124b9adbbebd97b803d7732bb8fdfdddd634
Pablo Argueta <pabloargueta27@gmail.com>
2018-06-19 03:04:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,8 @@
 # Add  code here!
 
 def prime?(integer)
-  
-  if integer <= 1 || integer % 2 == 0
-    return false
+  (2..(integer ** 0.5)).each do |num|
+    return false if integer % num == 0
   end
-
-n = 2
-
-while n < (integer**0.5)
-
-  return false if integer % n == 0
- 
- n += 1
- 
- end
- 
- true
-
-  
+  true
 end
