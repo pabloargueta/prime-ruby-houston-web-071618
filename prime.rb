12cbd2c20409676425dcec3985740c02016909ca
Pablo Argueta <pabloargueta27@gmail.com>
2018-06-19 02:27:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(integer)
  
  #create a range of numbers from 2 to integer  - 1
   
-  for i in 2..(interger - 1) do
+  for i in 2..(integer - 1) do
     if interger % i == 0
       return false
     end
