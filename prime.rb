4a6845c1a15e2b439b0c85595da90e37ab62a490
Pablo Argueta <pabloargueta27@gmail.com>
2018-06-19 02:33:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,11 +4,12 @@ def prime?(integer)
  
  #create a range of numbers from 2 to integer  - 1
   
-(2..(integer - 1)).each do |num|
-  return false if integer % num == 0
+  (2..(integer - 1)).each do |num|
+    
+    return false if integer % num == 0
+    
+  end
   
-end
-
-true
+  true
   
 end
